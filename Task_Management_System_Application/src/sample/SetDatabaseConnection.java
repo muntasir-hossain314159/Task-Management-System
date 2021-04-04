3865b051567794e757b5b40f4367fd2377d58faa
Muntasir Hossain <ahoss1@unh.newhaven.edu>
2021-04-04 14:48:00 -0400

    Application pushed (Java code)

---
+++ Task_Management_System_Application/src/sample/SetDatabaseConnection.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Set;
+
+public class SetDatabaseConnection {
+
+    public static Connection getConnection() throws Exception
+    {
+        try
+        {
+            String url = "jdbc:mysql://127.0.0.1:3306/Task_Management_System_DB";
+            String username = "root";
+            String password = "";
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
+            return conn;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+        return null;
+    }
+}
